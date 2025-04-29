e03e923578939a975d3a2bedbfbe2d17748f8176
bijaya tamang <kojingmoktan92@gmail.com>
2025-04-29 09:34:14 +0545

    An!meSky

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./SignUp.css";
+import "./Signup.css";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const SignUp = () => {
